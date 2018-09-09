85cbdc230f63798d3b47ccef640ce345722c0ffc
Your Name <you@example.com>
2018-09-09 07:29:48 -0300

    mejoras interaccionm

--- js/check/main.js
+++ js/check/main.js
@@ -78,7 +78,7 @@ angular.module('initApp')
 
     }
 
-    var invalid = []
+    var invalid = [];
     invalid.push("Largometraje Con I.N.C.A.A.");
     invalid.push("Cine Publicitario");
 
@@ -89,14 +89,24 @@ angular.module('initApp')
     invalid.push("Cortometraje");
     invalid.push("Documental");
     invalid.push("Largometraje Nacional");
-
+    
+    
     var valid = [];
-    // valid.push("Programa Televisivo");
-    // valid.push("Largometraje Nacional");
     valid.push("Largometraje");
     valid.push("Tv Ficcion");
     
-  	
+    var tv = [];
+    tv.push("Programa Televisivo");
+    tv.push("Tv Ficcion");
+  
+    var movies = [];
+    movies.push("Largometraje Con I.N.C.A.A.");
+    movies.push("Largometraje Nacional");
+    movies.push("Largometraje");
+
+    $rootScope.videoClips = [];
+    $rootScope.tv = [];
+    $rootScope.movies = [];
 
     d3.csv(url, function(data){
       $rootScope.$apply(function(){
@@ -108,11 +118,65 @@ angular.module('initApp')
                 $rootScope.readyToCheck.push(d);    
               }
             });
+            movies.map(function(m){
+              if (d['TIPO_PRODUCCION'] == m)  {
+                $rootScope.movies.push(d);    
+              }
+            });
+            tv.map(function(m){
+              if (d['TIPO_PRODUCCION'] == m)  {
+                $rootScope.tv.push(d);    
+              }
+            });
+            if (d.TIPO_PRODUCCION === 'Video Clip'){
+              $rootScope.videoClips.push(d);
+            }
+
         });
+        
+        data.map(function(d){
+          
+        });
+        $rootScope.netoTV= d3.map($rootScope.tv, function(d){return d.TITULO_PROYECTO.toLowerCase();}).keys();
+        $rootScope.netoVideoClips= d3.map($rootScope.videoClips, function(d){return d.TITULO_PROYECTO.toLowerCase();}).keys();
+        $rootScope.netoMovies= d3.map($rootScope.movies, function(d){return d.TITULO_PROYECTO.toLowerCase();}).keys();
         $rootScope.barrios = d3.map($rootScope.readyToCheck, function(d){return d.BARRIO.toLowerCase();}).keys();
         $rootScope.comunas = d3.map($rootScope.readyToCheck, function(d){return d.COMUNA.toLowerCase();}).keys();
+        $rootScope.netofechas = d3.map(data, function(d){return d.FECHA_RODAJE;}).keys();
+        $rootScope.netoPeriodo = d3.map(data, function(d){return d.PERIODO;}).keys();
+        $rootScope.netoProducciones = d3.map(data, function(d){return d.TITULO_PROYECTO;}).keys();
+        $rootScope.netoLocaciones = d3.map(data, function(d){return d.DIRECCION;}).keys();
         $rootScope.producciones = d3.map($rootScope.readyToCheck, function(d){return d.TITULO_PROYECTO;}).keys();
 
+
+        $rootScope.stats =[];
+        $rootScope.stats.push({
+          number: $rootScope.netoPeriodo.length,
+          label: 'Años'
+        });
+        $rootScope.stats.push({
+          number: $rootScope.netofechas.length,
+          label: 'Dias de Filmacion'
+        });
+        $rootScope.stats.push({
+          number: $rootScope.netoLocaciones.length,
+          label: 'Locaciones'
+        });
+        $rootScope.stats.push({
+          number: $rootScope.netoMovies.length,
+          label: 'Películas'
+        });
+        $rootScope.stats.push({
+          number: $rootScope.producciones.length,
+          label: 'Programas de TV'
+        }); 
+        
+        $rootScope.stats.push({
+          number: $rootScope.videoClips.length,
+          label: 'Video Clips'
+        });
+        
+        
         console.log($rootScope.barrios);
         console.log($rootScope.comunas);
         console.log($rootScope.producciones);
