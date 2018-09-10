f93c3944e049a4b22a93b04642d4757df87c6f32
Your Name <you@example.com>
2018-09-10 17:45:59 -0300

    magia con preguntas

--- js/check/main.js
+++ js/check/main.js
@@ -108,7 +108,7 @@ angular.module('initApp')
       //moveNext
       $location.path('pregunta/titulo');
     };
-    var setBarrioQuestion = function(){
+    var  setBarrioQuestion = function(){
       //Por Barrio
       var filteredAnswersBarrio = $rootScope.barrios.filter(function(d){
         return d != $rootScope.currentCheck.BARRIO.toLowerCase();
