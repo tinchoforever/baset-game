b0fb1bc883ab8652092aa3d95c00713c5dd9a9d3
Your Name <you@example.com>
2018-09-14 12:18:54 -0300

    mejoras visuals

--- js/mapa-ciudad.js
+++ js/mapa-ciudad.js
@@ -61,26 +61,20 @@ d3.xml("images/main.svg").mimeType("image/svg+xml").get(function(error, xml) {
           d3.selectAll('svg path.st1').transition()
             .duration(500).style("fill", "#d1d1d1")
           d3.select('svg #' + highlight.toUpperCase())
-              .transition().duration(200)
+              .transition().duration(1200)
               .style("fill", "#19c3e3")
-            .transition().duration(200)
-              .style("fill", "#fff9dc")
-            .transition().duration(200)
-              .style("fill", "#19c3e3")
+            
         }
+        if( window.highlightScope){
         d3.selectAll('svg path.st1').on('click',function(d){
           d3.selectAll('svg path.st1').transition()
             .duration(500).style("fill", "#d1d1d1")
           d3.select(this)
-              .transition().duration(200)
-              .style("fill", "#19c3e3")
-            .transition().duration(200)
-              .style("fill", "#fff9dc")
-            .transition().duration(200)
+             .transition().duration(1200)
               .style("fill", "#19c3e3")
           window.highlightScope(d3.select(this).attr('id'));
         });
-
+      }
 
   });
 
@@ -92,16 +86,13 @@ d3.xml("images/main.svg").mimeType("image/svg+xml").get(function(error, xml) {
 window.reloadGraph = function(data,highlight){
     // mainDataset = prepareData(data);
     // loadDataset(data);
+
     if (highlight){
           highlight = highlight.replace(' ', '_');
           d3.selectAll('svg path.st1').transition()
             .duration(500).style("fill", "#d1d1d1")
           d3.select('svg #' + highlight.toUpperCase())
-              .transition().duration(500)
-              .style("fill", "#19c3e3")
-            .transition().duration(1000)
-              .style("fill", "#fff9dc")
-            .transition().duration(5000)
+              .transition().duration(800)
               .style("fill", "#19c3e3")
         }
   }
